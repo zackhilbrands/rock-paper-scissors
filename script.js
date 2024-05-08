83f9b2fc31fa2375406ecde46834c55dae0568d1
zackhilbrands <zackhilbrands@hotmail.com>
2024-05-08 09:25:30 -0500

    added ui

--- script.js
+++ script.js
@@ -1,68 +1,136 @@
 let humanScore = 0
 let computerScore = 0
 
-function getComputerChoice() {
+const rockButton = document.querySelector(".rock")
+const paperButton = document.querySelector(".paper")
+const scissorsButton = document.querySelector(".scissors")
+const resultDiv = document.querySelector('.result')
+const humanScoreSpan = document.querySelector('.human-score')
+const computerScoreSpan = document.querySelector('.computer-score')
+
+const getComputerChoice = () => {
     const options = ["rock", "paper", "scissors"];
     const randomNum = Math.floor(Math.random () * 3)
-    const computerChoice = options[randomNum]
-    return computerChoice
+    return options[randomNum]
 }
 
 //console.log(getComputerChoice())
 
-function getHumanChoice() {
-    let humanChoice = prompt("Rock, Paper, or Scissors?");
-    humanChoice = humanChoice.toLowerCase();
-    return humanChoice;
-}
+//function getHumanChoice() {
+    //let humanChoice = prompt("Rock, Paper, or Scissors?");
+    //humanChoice = humanChoice.toLowerCase();
+    //return humanChoice;
+//}
 //function getHumanChoice is not necessary, the prompt can be put in the for loop, like this below
 // const humanSelection = prompt('Rock, Paper, or Scissors').toLowerCase()
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === 'rock' && computerChoice === 'rock') {
-        return 'Tie! Rock and Rock!'
+        const p = document.createElement('p')
+        p.innerText = 'Tie! Rock and Rock!' 
+        resultDiv.appendChild(p)
     }   else if (humanChoice === 'scissors' && computerChoice === 'scissors') {
-        return 'Tie! Scissors and Scissors'
+        const p = document.createElement('p')
+        p.innerText = 'Tie! Scissors and Scissors'
+        resultDiv.appendChild(p)
     }   else if (humanChoice === 'paper' && computerChoice === 'paper') {
-        return 'Tie! Paper and Paper'
+        const p = document.createElement('p')
+        p.innerText = 'Tie! Paper and Paper'
+        resultDiv.appendChild(p)
     }   else if (humanChoice === 'rock' && computerChoice === 'scissors') {
         humanScore++
-        return 'Victory! Rock crushes Scissors'
+        const p = document.createElement('p')
+        p.innerText = 'Victory! Rock crushes Scissors'
+        resultDiv.appendChild(p)
     }   else if (humanChoice === 'rock' && computerChoice === 'paper') {
         computerScore++
-        return 'Defeat... Paper from the top rope!'
+        const p = document.createElement('p')
+        p.innerText = 'Defeat... Paper from the top rope!'
+        resultDiv.appendChild(p)
     }   else if (humanChoice === 'paper' && computerChoice === 'rock') {
         humanScore++
-        return 'Victory! Rock? More like pebble!'
+        const p = document.createElement('p')
+        p.innerText = 'Victory! Rock? More like pebble!'
+        resultDiv.appendChild(p)
     }   else if (humanChoice === 'paper' && computerChoice === 'scissors') {
         computerScore++
-        return 'Defeat... Cut my life into pieces...'
+        const p = document.createElement('p')
+        p.innerText = 'Defeat... Cut my life into pieces...'
+        resultDiv.appendChild(p)
     }   else if (humanChoice === 'scissors' && computerChoice === 'paper') {
         humanScore++
-        return 'Victory! Scissors remain undefeated in this matchup'
+        const p = document.createElement('p')
+        p.innerText = 'Victory! Scissors remain undefeated in this matchup'
+        resultDiv.appendChild(p)
     }   else if (humanChoice === 'scissors' && computerChoice === 'rock') {
         computerScore++
-        return 'Defeat! Rock does what paper cannot!'
+        const p = document.createElement('p')
+        p.innerText = 'Defeat! Rock does what paper cannot!'
+        resultDiv.appendChild(p)
     }   
 }
 
+const logWinner = (humanScore, computerScore) =>{
+    if (humanScore === 5){
+        const h2 = document.createElement('h2')
+        h2.innerText = 'Computer defeated!'
+        resultDiv.append(h2)
+    }
 
-const playGame = () => {
-    for (let i =0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection))
+    else if (computerScore === 5) {
+        const h2 = document.createElement('h2')
+        h2.innerText = 'Human defeated!'
+        resultDiv.append(h2)
     }
-    if (humanScore > computerScore) {
-        return 'Human defeats Computer!'
-    }   else if (humanScore < computerScore) {
-        return 'Computer defeats Human!'
-    }   else {
-        return 'Tie!'
-    }
+
 }
 
-console.log(playGame())
+const updateScores = (humanScore, computerScore) => {
+    humanScoreSpan.innerText = `Human Score: ${humanScore}`
+    computerScoreSpan.innerText = `Computer Score: ${computerScore}`
+}
+
+rockButton.addEventListener('click', () => {
+    const computerSelection = getComputerChoice()
+    const playerSelection = 'rock'
+    playRound(playerSelection, computerSelection)
+    updateScores(humanScore, computerScore)
+    logWinner(humanScore, computerScore)
+    })
+
+paperButton.addEventListener('click', () =>{
+    const computerSelection = getComputerChoice()
+    const playerSelection = 'paper'
+    playRound(playerSelection, computerSelection)
+    updateScores(humanScore, computerScore)
+    logWinner(humanScore, computerScore)
+    })  
+    
+scissorsButton.addEventListener('click', () =>{
+    const computerSelection = getComputerChoice()
+    const playerSelection = 'scissors'
+    playRound(playerSelection, computerSelection)
+    updateScores(humanScore, computerScore)
+    logWinner(humanScore, computerScore)
+    })
+
+
+//const playGame = () => {
+    //for (let i =0; i < 5; i++) {
+        //const humanSelection = prompt('Rock, Paper, or Scissors').toLowerCase();
+        //const computerSelection = getComputerChoice();
+        //console.log(playRound(humanSelection, computerSelection))
+    //}
+    //if (humanScore > computerScore) {
+        //return 'Human defeats Computer!'
+    //}   else if (humanScore < computerScore) {
+        //return 'Computer defeats Human!'
+    //}   else {
+        //return 'Tie!'
+    //}
+//}
+
+//console.log(playGame())
 
 
 
